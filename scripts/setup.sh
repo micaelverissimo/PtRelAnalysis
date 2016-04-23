331d1466b651600f0f6c6ab81d027cbae23f1524
Isabella Silva Ferreira <micaelvero@hotmail.com>
2016-04-22 21:50:28 -0300

    Criando estruturas

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,3 +2,5 @@ export MY_PATH=$PWD
 cd /home/isabella/Workspace/python/PtRelAnalysis/functions/ArtificialIntelligence
 source setup.sh
 cd $MY_PATH
+
+export PYTHONPATH=$PYTHONPATH:/home/isabella/Workspace/python/PtRelAnalysis/PtRelAnalysis/functions
