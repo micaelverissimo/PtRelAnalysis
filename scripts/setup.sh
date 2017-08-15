fa8fe838f53695aae9d204eb8420eb641b54dc00
Micael Verissimo <micaelvero@hotmail.com>
2017-08-15 18:40:18 -0300

    KFold ready

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,9 @@
-export MY_PATH=$PWD
-cd /home/isabella/Workspace/python/PtRelAnalysis/functions/ArtificialIntelligence
-source setup.sh
-cd $MY_PATH
+#export MY_PATH=$PWD
+#cd /home/isabella/Workspace/python/PtRelAnalysis/functions/ArtificialIntelligence
+#source setup.sh
+#cd $MY_PATH
 
-export PYTHONPATH=$PYTHONPATH:/home/isabella/Workspace/python/PtRelAnalysis/PtRelAnalysis/
+export PYTHONPATH=$PYTHONPATH:/home/micael/MyWorkspace/PtRelAnalysis/functions
 
 echo
 echo "Now is everything all right"
